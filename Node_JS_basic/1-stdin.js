8de56d3b531ecc7cd6ee7f10317caf9dbabed5f9
mistWil <7448@holbertonstudents.com>
2024-05-22 12:51:05 +0200

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
-});
-
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+const string = 'Welcome to Holberton School, what is your name?';
+process.stdout.write(`${string}\n`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+    process.stdout.write('This important software is now closing\n');
+  }
 });
