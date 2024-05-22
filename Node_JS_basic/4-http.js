4e7b84e8538840161ae4ff5f44ae57c0276fca05
mistWil <7448@holbertonstudents.com>
2024-05-22 12:56:28 +0200

    4-http.js

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
