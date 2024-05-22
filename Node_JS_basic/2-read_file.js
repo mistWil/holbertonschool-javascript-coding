22b416d1e4f3d1497385378a0006ba819da49706
mistWil <7448@holbertonstudents.com>
2024-05-22 12:53:45 +0200

    2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,33 +1,34 @@
 const fs = require('fs');
 
-function countStudents(path) {
+module.exports = function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8').trim().split('\n');
-    const students = data.filter((student) => student);
-    const fields = {};
-    const fieldNames = {};
+    const content = fs.readFileSync(path, 'utf8').trim();
+    const lines = content.split('\n');
 
-    students.forEach((student) => {
-      const [firstName, field] = student.split(',');
-      if (fields[field]) {
-        fields[field].push(firstName);
-      } else {
-        fields[field] = [firstName];
-        fieldNames[field] = field;
-      }
-    });
+    let sweCount = 0;
+    let csCount = 0;
 
-    console.log(`Number of students: ${students.length}`);
-    for (const field in fieldNames) {
-      if (Object.prototype.hasOwnProperty.call(fieldNames, field)) {
-        console.log(
-          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
-        );
+    const sweList = [];
+    const csList = [];
+
+    for (const line of lines) {
+      const fields = line.split(',');
+      const [firstName, lastName, age, field] = fields;
+      if (firstName && lastName && age && field) {
+        if (field === 'CS') {
+          csCount += 1;
+          csList.push(firstName);
+        } else if (field === 'SWE') {
+          sweCount += 1;
+          sweList.push(firstName);
+        }
       }
     }
+
+    console.log(`Number of students: ${csCount + sweCount}`);
+    console.log(`Number of students in CS: ${csCount}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
-}
-
-module.exports = countStudents;
+};
